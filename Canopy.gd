b31d36f816ce25f960d95fcbbb0f5782bcc7b68e
Kaleb Clint <kalebclintmail@gmail.com>
2023-12-09 13:25:41 +1300

    started to create basic Shade/Light Mechanics.

---
+++ Canopy.gd
@@ -0,0 +1,25 @@
+extends StaticBody3D
+
+@onready var player = $"../Player"
+@onready var GameManager = $".."
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_shade_collision_body_entered(body):
+	#Check if the object if the player, if so then thye are in shade
+	if body.name == "Player" && GameManager.sunsunOverhead:
+		player.underShade = true
+
+func _on_shade_collision_body_exited(body):
+	#Check if the object if the player, if so then they left shade
+	if body.name == "Player" && GameManager.sunsunOverhead:
+		player.underShade = false
